bfd3d01d7b07f4f5e7838dc6eb9e8ef795dae4ab
eugeneblog <714284150@qq.com>
2018-09-07 15:14:25 +0800

    add-layout

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -2,7 +2,7 @@
 Ext.define('Bitcoin.view.main.Main',{
     extend: 'Ext.container.Viewport',
     requires: [
-        
+        'Ext.button.Segmented', 'Ext.list.Tree'
     ],
     controller: 'main',
     viewModel: 'main',
@@ -12,9 +12,12 @@ Ext.define('Bitcoin.view.main.Main',{
         //子试图铺满容器
         align: 'stretch'
     },
-
+    listeners: {
+        render: 'onMainViewRender'
+    },
     items:[
         {
+            //上方容器
             xtype: 'toolbar',
             cls: 'sencha-dash-dash-headerbar shadow',
             height: 64,
@@ -72,6 +75,72 @@ Ext.define('Bitcoin.view.main.Main',{
                      }
                 }
             ]
+        },{
+            //下方容器
+            xtype: 'container',
+            id: 'main-view-detail-wrap',
+            reference: 'mainContainerWrap',   //别名，方便查询
+            flex: 1,      
+            hidden: true,
+            bind: {
+                hidden: '{isHideMain}'   //绑定数据
+            },
+            layout: {       //布局方式
+                type: 'hbox',  //水平布局
+                //是否支持动画效果
+                //用于支持菜单栏折叠/展开动画效果
+                animate: true,
+                animatePolicy: {
+                    x: true,
+                    width: true
+                }
+            },
+            items: [{
+                    height: "100%",
+                    scrollable: 'y',
+                    reference: 'navigationContainer',
+                    cls: 'navigationContainer',
+                    xtype: 'container',
+                    width: 250,
+                    items: [{
+                            xtype: 'treelist',
+                            reference: 'navigationTreeList',
+                            itemId: 'navigationTreeList',
+                            ui: 'nav',
+                            //注意第四章的时候这里需要去掉注释
+                            //store: 'navigationTree',
+                            width: 250,
+                            //展开按钮显示在右侧
+                            expanderFirst: false,
+                            //点击父菜单任何区域都可展开子菜单
+                            expanderOnly: false,
+                            //只有一个节点能展开
+                            singleExpand: true,
+                            listeners: {
+                                //监听导航菜单选中改变事件
+                                selectionchange: 'onNavigationTreeSelectionChange'
+                            }
+                    }]
+                },{
+                    //内容展示模块  
+                    xtype: 'container',     //这个容器作为主要展示区域
+                    height: '100%',
+                    flex: 1,
+                    reference: 'mainCardPanel',
+                    itemId: 'contentPanel',
+                    //返回页面集合，自定义属性
+                    backView: [],
+                    layout: {
+                        //跑马灯布局
+                        type: 'card',
+                        //暂时不知道用处
+                        anchor: '100%'
+                    },
+                    //子item默认配置
+                    defaults: {
+                        padding: 20
+                    }
+                }]
         }
     ]
 })
